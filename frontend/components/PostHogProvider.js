f4c2a3d2d1e2eb325a0fb6f31e418c6f7144e081
AdhwaithAS <adhwaithanujas@gmail.com>
2026-06-24 03:34:23 +0530

    Update PostHogProvider.js

--- frontend/components/PostHogProvider.js
+++ frontend/components/PostHogProvider.js
@@ -2,7 +2,7 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
 function PostHogPageView() {
@@ -33,7 +33,9 @@ export default function PostHogProvider({ children }) {
 
   return (
     <PHProvider client={posthog}>
-      <PostHogPageView />
+      <Suspense fallback={null}>
+        <PostHogPageView />
+      </Suspense>
       {children}
     </PHProvider>
   );
